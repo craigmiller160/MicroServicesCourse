5911a16047144defd9bf40c154e3fa0dd3170ac0
Craig Miller <craigmiller160@gmail.com>
2020-03-28 09:48:55 -0400

    Added config-server

---
+++ config-server/src/main/java/com/krishantha/rentcloud/configserver/ConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.krishantha.rentcloud.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+
+}
