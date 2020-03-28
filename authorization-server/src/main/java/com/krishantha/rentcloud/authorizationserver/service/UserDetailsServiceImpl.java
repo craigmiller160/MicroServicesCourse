ecf6ee8745398e2edfe35e9a73ca13e90fe8b311
Craig Miller <craigmiller160@gmail.com>
2020-03-28 19:46:56 -0400

    Added AccountStatusUserDetailsChecker

--- authorization-server/src/main/java/com/krishantha/rentcloud/authorizationserver/service/UserDetailsServiceImpl.java
+++ authorization-server/src/main/java/com/krishantha/rentcloud/authorizationserver/service/UserDetailsServiceImpl.java
@@ -3,6 +3,7 @@ package com.krishantha.rentcloud.authorizationserver.service;
 import com.krishantha.rentcloud.authorizationserver.entity.User;
 import com.krishantha.rentcloud.authorizationserver.model.AuthUser;
 import com.krishantha.rentcloud.authorizationserver.repository.UserRepository;
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -21,7 +22,9 @@ public class UserDetailsServiceImpl implements UserDetailsService {
     public UserDetails loadUserByUsername(final String name) throws UsernameNotFoundException {
         final User user = userRepository.findByUsername(name)
                 .orElseThrow(() -> new UsernameNotFoundException("Couldn't find user with name " + name));
-        return new AuthUser(user);
+        final var authUser = new AuthUser(user);
+        new AccountStatusUserDetailsChecker().check(authUser);
+        return authUser;
     }
 
 }
