d71dcf7bb7866b47c171e8ecd46dc46fb55748fa
Craig Miller <craigmiller160@gmail.com>
2020-03-28 15:42:55 -0400

    Code cleanup

--- profile-service/src/main/java/com/krishantha/rentcloud/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/krishantha/rentcloud/profileservice/controller/ProfileController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.swing.text.html.Option;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/services")
