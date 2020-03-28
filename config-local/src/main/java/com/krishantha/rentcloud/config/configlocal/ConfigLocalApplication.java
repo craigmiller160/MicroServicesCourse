23041da5d384c984a990476275b03d867c8b9fb0
Craig Miller <craigmiller160@gmail.com>
2020-03-28 08:48:54 -0400

    Created config-local

---
+++ config-local/src/main/java/com/krishantha/rentcloud/config/configlocal/ConfigLocalApplication.java
@@ -0,0 +1,13 @@
+package com.krishantha.rentcloud.config.configlocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigLocalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigLocalApplication.class, args);
+    }
+
+}
