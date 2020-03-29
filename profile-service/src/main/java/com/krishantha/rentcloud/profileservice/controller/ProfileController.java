7faa5db7e57be76688e391e4c3cfde4592da612d
Craig Miller <craigmiller160@gmail.com>
2020-03-29 10:10:47 -0400

    Added method level security

--- profile-service/src/main/java/com/krishantha/rentcloud/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/krishantha/rentcloud/profileservice/controller/ProfileController.java
@@ -3,6 +3,7 @@ package com.krishantha.rentcloud.profileservice.controller;
 import com.krishantha.rentcloud.profileservice.model.Customer;
 import com.krishantha.rentcloud.profileservice.service.CustomerService;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,16 +24,19 @@ public class ProfileController {
     }
 
     @PostMapping("/customer")
+    @PreAuthorize("hasAuthority('create_profile')")
     public Customer saveCustomer(@RequestBody final Customer customer) {
         return this.customerService.save(customer);
     }
 
     @GetMapping("/customer")
+    @PreAuthorize("hasAuthority('read_profile')")
     public List<Customer> getAllCustomers() {
         return customerService.getAllCustomers();
     }
 
     @GetMapping("/customer/{id}")
+    @PreAuthorize("hasAuthority('read_profile')")
     public ResponseEntity<Customer> getCustomer(@PathVariable final Long id) {
         return customerService.getCustomer(id)
                 .map(ResponseEntity::ok)
