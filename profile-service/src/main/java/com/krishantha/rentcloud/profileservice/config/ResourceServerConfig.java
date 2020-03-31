9a1365834f60303e72cdf6d86a59bbff978a6ee8
Craig Miller <craigmiller160@gmail.com>
2020-03-31 17:13:00 -0400

    Updated resourceId

--- profile-service/src/main/java/com/krishantha/rentcloud/profileservice/config/ResourceServerConfig.java
+++ profile-service/src/main/java/com/krishantha/rentcloud/profileservice/config/ResourceServerConfig.java
@@ -1,18 +1,29 @@
 package com.krishantha.rentcloud.profileservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.util.StringUtils;
 
 @Configuration
 @EnableResourceServer
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
+    private final String resourceId;
+
+    public ResourceServerConfig(@Value("${security.oauth2.resource.resourceId}") final String resourceId) {
+        this.resourceId = resourceId;
+    }
+
     @Override
-    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+    public void configure(final ResourceServerSecurityConfigurer resources) throws Exception {
+        if (StringUtils.isEmpty(resourceId)) {
+            throw new Exception("Cannot create resource server without resourceId");
+        }
         resources.resourceId("profile");
     }
 }
