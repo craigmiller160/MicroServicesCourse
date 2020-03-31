f0b18cd3e4f090a10a62db86a937173cc210e434
Craig Miller <craigmiller160@gmail.com>
2020-03-31 16:52:46 -0400

    Added configs to resource server

---
+++ profile-service/src/main/java/com/krishantha/rentcloud/profileservice/config/ResourceServerConfig.java
@@ -0,0 +1,18 @@
+package com.krishantha.rentcloud.profileservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+
+@Configuration
+@EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
+        resources.resourceId("profile");
+    }
+}
