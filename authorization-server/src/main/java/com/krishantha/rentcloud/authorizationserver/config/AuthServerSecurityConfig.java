2c3d4ef5063ddbfecdca09a3c090fa081ca809ed
Craig Miller <craigmiller160@gmail.com>
2020-04-25 21:19:56 -0400

    Got the app running again

--- authorization-server/src/main/java/com/krishantha/rentcloud/authorizationserver/config/AuthServerSecurityConfig.java
+++ authorization-server/src/main/java/com/krishantha/rentcloud/authorizationserver/config/AuthServerSecurityConfig.java
@@ -1,17 +1,25 @@
 package com.krishantha.rentcloud.authorizationserver.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerSecurityConfiguration;
 
 @Configuration
-@Order(100)
+@Order(200)
 public class AuthServerSecurityConfig extends AuthorizationServerSecurityConfiguration {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.authorizeRequests().anyRequest().permitAll();
+        http
+                .requestMatchers()
+                .antMatchers("/jwk")
+                .and()
+                .authorizeRequests().anyRequest().permitAll();
     }
 
 }
